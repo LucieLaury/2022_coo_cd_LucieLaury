662071d94c489d1ec8bc9a0379e3420233af4e03
Lucie <lucye.laury@gmail.com>
2022-05-10 11:33:49 +0200

    Q3 à Q5 commencement de l'ecriture de test 01, magasin valide

---
+++ test/TestChargeurMagasin.java
@@ -0,0 +1,21 @@
+import XML.ChargeurMagasin;
+import org.junit.jupiter.api.Test;
+
+public class TestChargeurMagasin {
+
+    @Test
+    public void t01_chargerMagasin_valide (){
+        //préparation des données
+        ChargeurMagasin cm = new ChargeurMagasin("musicbrainzSimple");
+        //méthode testée
+        //vérification
+    }
+
+    @Test
+    public void t02_chargerMagasin_nonValide (){
+        //préparation des données
+        //méthode testée
+        //vérification
+    }
+
+}
